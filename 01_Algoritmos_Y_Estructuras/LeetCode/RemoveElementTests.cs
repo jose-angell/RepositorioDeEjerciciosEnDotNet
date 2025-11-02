3b7862f5551d8b47bf37845542199630accc34c0
jose Angel <josegallardo.cor@gmail.com>
2025-11-02 01:05:25 -0600

    feat: test de remove element

---
+++ 01_Algoritmos_Y_Estructuras/LeetCode/RemoveElementTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Algoritmos_Y_Estructuras.LeetCode
+{
+    public class RemoveElementTests
+    {
+        [Fact]
+        public void RemoveElement_CasoBase()
+        {
+            // Arrange
+            int[] nums = { 1,2,3,4,5,6,4,32,1};
+            int val = 1;
+            int expected = 7;
+            // Act
+            var act = new RemoveElement();
+            var result = act.Solution(nums, val);
+
+            // Assert
+            Assert.Equal(result, expected);
+
+        }
+        [Fact]
+        public void RemoveElement_CasoNoTieneNingunValorDiferenteDeVal()
+        {
+            // Arrange
+            int[] nums = { 1,1,1,1,1,1,1,1 };
+            int val = 1;
+            int expected = 0;
+            // Act
+            var act = new RemoveElement();
+            var result = act.Solution(nums, val);
+
+            // Assert
+            Assert.Equal(result, expected);
+
+        }
+        [Fact]
+        public void RemoveElement_CasoTodosSonDiferentesDeVal()
+        {
+            // Arrange
+            int[] nums = { 1,2,3,4,5,6,7};
+            int val = 0;
+            int expected = 6;
+            // Act
+            var act = new RemoveElement();
+            var result = act.Solution(nums, val);
+
+            // Assert
+            Assert.Equal(result, expected);
+
+        }
+        [Fact]
+        public void RemoveElement_CasoNumsEsNull()
+        {
+            // Arrange
+            int[] nums = null;
+            int val = 0;
+            int expected = 0;
+            // Act
+            var act = new RemoveElement();
+            var result = act.Solution(nums, val);
+
+            // Assert
+            Assert.Equal(result, expected);
+
+        }
+    }
+}
