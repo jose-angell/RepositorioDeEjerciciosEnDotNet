53d93f546994c47d11508cba97ec7df4ba98d949
jose Angel <josegallardo.cor@gmail.com>
2025-11-03 23:43:04 -0600

    feat: ejercicio is palindrome number, con pruebas

---
+++ 01-Algoritmos_Y_Estructuras/LeetCode/IsPalindromeNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Is Palindrome Number
+ * Validar si el numero x es un palidromo
+ * Entrada x un numero entero, 
+ * salida true o false, 
+ * 
+ */
+namespace _01_Algoritmos_Y_Estructuras.LeetCode
+{
+    public class IsPalindromeNumber
+    {
+        public bool Solution(int x)
+        {
+            string val = x.ToString();
+            int left = 0;
+            int right = val.Length;
+            while (left <= right)
+            {
+                if (val[left] != val[right])
+                {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+    }
+}
