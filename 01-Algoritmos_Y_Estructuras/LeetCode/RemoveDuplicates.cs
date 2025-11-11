a8f01bb38ff89c057be8f9c4029f865f9ff99d57
jose Angel <josegallardo.cor@gmail.com>
2025-11-10 23:57:22 -0600

    feat: remove duplicates y  tests

---
+++ 01-Algoritmos_Y_Estructuras/LeetCode/RemoveDuplicates.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 26. Remove Duplicates from Sorted Array
+ * Dada una lista de numeros enteros ordenados eliminar los valores duplicados en la misma lista de entrada
+ * la salida esperada debe ser un valor entero que indique el tamano de la lista sin duplicados.
+ * Input: int[] nums lista de enteros ordenada
+ * Output: int numero entero que representa el numero de elementos unicos dentro de la lista
+ * 
+ * Complegindad de O(1) sin usar espacio extra
+ * 
+ * Solucion
+ * 1. Iniciar k en 1 ya que el primer elemento siempre esta ordenado y es unico al ser solo uno
+ * 2. Declarar un ciclo for i que comience en 1 y recorra el array hasta el final,
+ * 3. validar si el elemento actual diferente al anterior entonces el actual no esta duplicado y se suma uno a k
+ *    y se guarda la en la posicion k el valor en la posicion i
+ * 4. al terminar el ciclo se retorna k.
+ * 
+ */
+namespace _01_Algoritmos_Y_Estructuras.LeetCode
+{
+    public class RemoveDuplicates
+    {
+        public int Solution(int[] nums)
+        {
+            if(nums.Length == 0) return 0;
+            int k = 1;
+            for(int i =1; i< nums.Length; i++)
+            {
+                if (nums[i - 1] != nums[i])
+                {
+                    nums[k++] = nums[i];
+                }
+            }
+            return k;
+        }
+    }
+}
---
+++ 01_Algoritmos_Y_Estructuras/LeetCode/RemoveDuplicatesTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Algoritmos_Y_Estructuras.LeetCode
+{
+    public class RemoveDuplicatesTests
+    {
+
+        [Theory]
+        [InlineData(new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4})]
+        public void RemoveDuplicates_returnConDuplicados(int[] nums)
+        {
+            // Act
+            var act = new RemoveDuplicates();
+            var result = act.Solution(nums);
+
+            // Assert
+            Assert.True(result < nums.Length);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 })]
+        public void RemoveDuplicates_returnSinDuplicados(int[] nums)
+        {
+            // Act
+            var act = new RemoveDuplicates();
+            var result = act.Solution(nums);
+
+            // Assert
+            Assert.True(result == nums.Length);
+        }
+    }
+}
