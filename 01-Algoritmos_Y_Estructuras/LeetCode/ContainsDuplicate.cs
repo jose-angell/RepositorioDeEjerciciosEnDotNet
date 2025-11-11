7067da8d14f652c019ada321b1cefdae38a3b113
jose Angel <josegallardo.cor@gmail.com>
2025-11-10 22:51:05 -0600

    feat :ContainDuplicate con tests

---
+++ 01-Algoritmos_Y_Estructuras/LeetCode/ContainsDuplicate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Contains Duplicate
+ * Determinar si la lista de enteros posee numeros duplicados
+ * Input: lista de numeros enteros
+ * output: booleano 
+ * Restricciones: 1 <= nums.Length <= 10^5,  -10^9 <= nums[i] <= 10^9
+ * 
+ * Solucion fuerza bruta
+ * 1. establecer un ciclo for principal, i para recorresr la lista de enteros
+ * 2. establecer un ciclo for anidado, j para recorrer la lista y comparar el elmento en i con dad elemento en j
+ * 3. (Dentro del ciclo) condicional, i y j deben ser diferente posicion, si el elemento en j es igual al elemento en i
+ *    retorna true para indicar que exite duplicados,
+ * 4. Si el ciclo principal termina y no se encotro duplicados retorna flase.
+ * 
+ * Complegidad inical de O(n^2) y espacial de O(1)
+ * 
+ * cuello de botella: el tener un bucle anidado genera un costo de tiempo significativo cuando las entradas son grandes
+ * Patron:
+ * implementar Hashing (HashSet en C#) uso de un HashSet para verificacion de existencia en tiempo O(1), logrando una
+ * solucion final O(N)
+ * 
+ */
+namespace _01_Algoritmos_Y_Estructuras.LeetCode
+{
+    public class ContainsDuplicate
+    {
+        public bool Solution(int[] nums)
+        {
+            HashSet<int> numSet = new HashSet<int>(nums);
+            return nums.Length != numSet.Count;
+        }
+    }
+}
---
+++ 01_Algoritmos_Y_Estructuras/LeetCode/ContainsDuplicateTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Algoritmos_Y_Estructuras.LeetCode
+{
+    public class ContainsDuplicateTests
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3, 1, 4, 5, 6, 7, 8, 9, 0 })]
+        [InlineData(new int[] { 11, 22, 33, 44, 55, 66, 77, 88, 99, 10, 1, 2, 3, 4, 5, 6, 8, 9, 0, 11 })]
+        [InlineData(new int[] { 12, 23, 43, 64, 75, 86, 97, 80, 12, 19, 20 })]
+        [InlineData(new int[] { 12, 23, 34, 40, 45, 55, 66, 17, 28, 39, 40 })]
+        public void ContainsDuplicate_returnTrue(int[] nums)
+        {
+            // act
+            var act = new ContainsDuplicate();
+            var result = act.Solution(nums);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData(new int[] {1,2,3,4,5,6,7,8,9,0 })]
+        [InlineData(new int[] {11,22,33,44,55,66,77,88,99,10,1,2,3,4,5,6,8,9,0 })]
+        [InlineData(new int[] { 12, 23, 43, 64, 75, 86, 97, 80, 19, 20 })]
+        [InlineData(new int[] { 12, 23, 34, 45, 55, 66, 17, 28, 39, 40 })]
+        public void ContainsDuplicate_returnFalse(int[] nums)
+        {
+            // act
+            var act = new ContainsDuplicate();
+            var result = act.Solution(nums);
+
+            // Assert
+            Assert.False(result);
+        }
+    }
+}
