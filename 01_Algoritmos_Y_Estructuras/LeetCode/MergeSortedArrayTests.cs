4584e749858a65429731169f14ad872a7bd8314a
jose Angel <josegallardo.cor@gmail.com>
2025-11-25 23:43:45 -0600

    feat: sea gregaron nuevos casos de rpueba para mergeSortedArray

--- 01_Algoritmos_Y_Estructuras/LeetCode/MergeSortedArrayTests.cs
+++ 01_Algoritmos_Y_Estructuras/LeetCode/MergeSortedArrayTests.cs
@@ -13,6 +13,15 @@ namespace _01_Algoritmos_Y_Estructuras.LeetCode
         [InlineData(new int[] { 1 }, 1, new int[] {}, 0, new int[] { 1 })]
         [InlineData(new int[] { 0 }, 0, new int[] { 1 }, 1, new int[] { 1 })]
         [InlineData(new int[] { 3,4,6, 0, 0, 0 }, 3, new int[] { 2, 5, 6 }, 3, new int[] { 2, 3, 4, 5, 6, 6 })]
+
+        // Casos Límite y Fusión Inversa (nums1 es mayor)
+        [InlineData(new int[] { 4, 5, 6, 0, 0, 0 }, 3, new int[] { 1, 2, 3 }, 3, new int[] { 1, 2, 3, 4, 5, 6 })]
+
+        // Caso de Igualdad y Duplicados
+        [InlineData(new int[] { 1, 1, 1, 0, 0, 0 }, 3, new int[] { 1, 1, 1 }, 3, new int[] { 1, 1, 1, 1, 1, 1 })]
+
+        // Caso Mínimo
+        [InlineData(new int[] { 1, 0 }, 1, new int[] { 2 }, 1, new int[] { 1, 2 })]
         public void MergeSortedArray_withSucces(int[] nums1, int m, int[] nums2, int n, int[] mergeArray)
         {
             // Act
