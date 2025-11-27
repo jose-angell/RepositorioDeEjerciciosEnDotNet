c87e9b2dc79afcba0ac6c01b3ea22aafaa8255ca
jose Angel <josegallardo.cor@gmail.com>
2025-11-27 17:59:38 -0600

    feat: Reverse string solucion y tests

---
+++ 01-Algoritmos_Y_Estructuras/LeetCode/ReverseString.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* 344 Reverse String
+ * Dado una arreglo  de caracteres, invertir el arreglo in-place.
+ * Importante: No se debe asignar otro arreglo, debe modificar el arreglo dado.
+ * Imput: ['h','e','l','l','o']
+ * Output: ['o','l','l','e','h']
+ * 
+ * Solucion: O(n) tiempo y O(1) espacio
+ * - usar dos punteros, uno al inicio y otro al final del arreglo
+ * - Inicializar un bucle que continue hasta que los punteros se crucen
+ * - Intercambiar los caracteres en la posicion de los punteros
+ * 
+ */
+namespace _01_Algoritmos_Y_Estructuras.LeetCode
+{
+    public class ReverseString
+    {
+        public void Solution(char[] s)
+        {
+            int start = 0;
+            int end = s.Length - 1;
+            while (start < end)
+            {
+                char aux = s[start];
+                s[start] = s[end];
+                s[end] = aux;
+
+                start += 1;
+                end -= 1;
+            }
+        }
+    }
+}
---
+++ 01_Algoritmos_Y_Estructuras/LeetCode/ReverseStringTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Algoritmos_Y_Estructuras.LeetCode
+{
+    public class ReverseStringTests
+    {
+        [Theory]
+        [InlineData(new char[] { 'h', 'e', 'l', 'l', 'o' }, new char[] { 'o', 'l', 'l', 'e', 'h' })]
+        [InlineData(new char[] { 'H', 'a', 'n', 'n', 'a', 'h' }, new char[] { 'h', 'a', 'n', 'n', 'a', 'H' })]
+        [InlineData(new char[] { 'H' }, new char[] { 'H' })]
+        [InlineData(new char[] { 'a', 'b', 'c' }, new char[] { 'c', 'b', 'a' })]
+
+        public void ReverseString_withSucces(char[] s, char[] expected)
+        {
+            // Act
+            var act = new ReverseString();
+            act.Solution(s);
+            // Assert
+            Assert.Equal(expected, s);
+        }
+    }
+}
