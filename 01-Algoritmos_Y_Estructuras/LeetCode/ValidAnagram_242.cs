ecca8ff0a271de8bfdd37e5656c3826093a22e55
jose Angel <josegallardo.cor@gmail.com>
2026-01-24 23:23:47 -0600

    feat: ejercicio 242 valida anagrama

---
+++ 01-Algoritmos_Y_Estructuras/LeetCode/ValidAnagram_242.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// 242. Valid Anagram
+/* Objetivo: teniendo como entrada dos string s y t, determinar si t es un anagrama de s
+ * Entrada : s y t -> string
+ * salida : booleano -> true si t es un anagrama de s, false en caso contrario
+ *  
+ */
+namespace _01_Algoritmos_Y_Estructuras.LeetCode
+{
+    public class ValidAnagram_242
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            char[] ss = s.ToCharArray();
+            char[] tt = t.ToCharArray();
+            Array.Sort(ss);
+            Array.Sort(tt);
+            return new string(ss) == new string(tt);
+        }
+    }
+}
