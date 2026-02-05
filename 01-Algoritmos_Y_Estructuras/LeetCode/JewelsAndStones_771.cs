01a179276bea5d5a8fb4414e57873809a4d556bb
jose Angel <josegallardo.cor@gmail.com>
2026-02-05 17:51:19 -0600

    feat: solucion paraa ejercicio 771 jewels and stones

---
+++ 01-Algoritmos_Y_Estructuras/LeetCode/JewelsAndStones_771.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/* 771 Jewels and Stones
+ * Dado dos arrays de caracteres jewels y stones, donde jewels representa los tipos de piedras preciosas, y stones representa las piedras que tienes en toltal,
+ * contar cuantas priedas en stones son tambien joyas en jewels.
+ * Input: jewels = "aA", stones = "aAAbbbb"
+ * Output: 3    
+ * Solucion O(n) tiempo y O(m) espacio adicional 
+ * 1. Definir un HashSet para almacenar los tipos de joyas.
+ * 2.Inicializar un contador en cero.
+ * 3. Recorrer cada piedra en stones y verificar si es una joya.
+ * 4. Si es una joya, incrementar el contador.
+ * 5. Retornar el contador.
+ * 
+ * 
+ */
+
+namespace _01_Algoritmos_Y_Estructuras.LeetCode
+{
+    public class JewelsAndStones_771
+    {
+        public int NumJewelsInStones(string jewels, string stones)
+        {
+            var hashJewels = new HashSet<char>(jewels);
+            int count = 0;
+            foreach (char s in stones)
+            {
+                if (hashJewels.Contains(s))
+                {
+                    count += 1;
+                }
+            }
+            return count;
+        }
+    }
+}
