66ea35f158e4ce016c774b673303851a40371186
jose Angel <josegallardo.cor@gmail.com>
2026-02-15 21:12:01 -0600

    feat: solucion 1832

---
+++ 01-Algoritmos_Y_Estructuras/LeetCode/CheckIfSentenceIsParagram_1832.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 1832. Check if the Sentence is Pangram
+ * Dado una entrada de tipo string validar si todas las letras del alfabeto aparecen almenos una vez
+ * Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
+ * Output: true
+ * Solucion: O(n) y espacio O(n)
+ * 1. Inicializar un hashSet con los elementos del string de entrada
+ * 2. crear una variable que almacene todos los elementos del alfabeto
+ * 3. Recorrer el alfabeto y validar si aparece en el hashset del string de entrada, si no lo hace retornar false
+ * 4. Si el ciclo termina retornar true.
+ */
+
+namespace _01_Algoritmos_Y_Estructuras.LeetCode
+{
+    public class CheckIfSentenceIsParagram_1832
+    {
+        public bool CheckIfPangram(string sentence)
+        {
+            HashSet<char> sens = new(sentence);
+            string alpha = "qwertyuiopasdfghjklzxcvbnm";
+            foreach (char a in alpha)
+            {
+                if (!sens.Contains(a))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
