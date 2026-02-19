281addf625f7e23842e25fffcdec78637787d893
jose Angel <josegallardo.cor@gmail.com>
2026-02-19 00:47:16 -0600

    feat: solucion ejercicio 2351 first letter to appear twice

---
+++ 01-Algoritmos_Y_Estructuras/LeetCode/FirstLetterToAppearTwice_2351.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 2351. First Letter to Appear Twice
+ * Dado un string de letras minusculas encontrar la primera letra que aparece dos veces
+ * 
+ * Input: s = "abccbaacz"
+ * Output: "c"
+ * 
+ * Solucion: O(n) y espacio O(n)
+ * 1. Inicializar un hashset para guardar las letras que ya han aparecido
+ * 2. Recorrer el string de entrada y verificar si la letra ya esta en el hashset, si esta, retornar esa letra
+ * 
+ * 
+ * 
+ */
+
+namespace _01_Algoritmos_Y_Estructuras.LeetCode
+{
+    public class FirstLetterToAppearTwice_2351
+    {
+        public char RepeatedCharacter(string s)
+        {
+            HashSet<char> twiceSet = new HashSet<char>();
+            foreach (char letter in s)
+            {
+                if (twiceSet.Contains(letter))
+                {
+                    return letter;
+                }
+                twiceSet.Add(letter);
+            }
+            return new char();
+        }
+    }
+}
