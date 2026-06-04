f9b70628852c6b426f345d4d855e05c5336397fc
jose Angel <josegallardo.cor@gmail.com>
2026-06-03 23:29:06 -0600

    feat: solucion ejercicio 1021

---
+++ 01-Algoritmos_Y_Estructuras/LeetCode/Stack/RemoveOutermostParentheses_1021.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Algoritmos_Y_Estructuras.LeetCode.Stack
+{
+    public class RemoveOutermostParentheses_1021
+    {
+        public string RemoveOuterParentheses(string s)
+        {
+            int open = 0;
+            StringBuilder result = new StringBuilder();
+            foreach (char c in s) { 
+                if(c == '(')
+                {
+                    if(open > 0)
+                    {
+                        result.Append(c);
+                    }
+                    open++;
+                }
+                else if(c == ')')
+                {
+                    open--;
+                    if(open > 0)
+                    {
+                        result.Append(c);
+                    }
+                }
+            }
+            return result.ToString();
+        }
+    }
+}
