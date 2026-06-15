f54b27a2eeb09ad25510ada748eca99bf0ffbe61
jose Angel <josegallardo.cor@gmail.com>
2026-06-14 23:40:23 -0600

    feat: solucion ejercicio 1614

---
+++ 01-Algoritmos_Y_Estructuras/LeetCode/Stack/MaximumNestingDepthParentheses_1614.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 1614. Maximum Nesting Depth of the Parentheses
+ * Dado una cadena s que consiste en paréntesis redondos '(' y ')', encuentra la profundidad máxima de anidamiento de los paréntesis redondos.
+ * Solucion
+ * 1. Inicializamos dos variables, count y max, para llevar un seguimiento de la profundidad actual y la profundidad máxima respectivamente.
+ * 2. Iteramos a través de cada carácter en la cadena s:
+ * 3. Si el carácter es '(', incrementamos count en 1, lo que indica que hemos entrado en un nuevo nivel de anidamiento.
+ * 4. Si el carácter es ')', decrementamos count en 1, lo que indica que hemos salido de un nivel de anidamiento.
+ * 5. Después de cada incremento o decremento de count, verificamos si count es mayor que max. Si es así, actualizamos max con el nuevo valor de count.
+ * 6. Al final de la iteración, max contendrá la profundidad máxima de anidamiento de los paréntesis redondos en la cadena s, que es lo que devolvemos como resultado.
+ */
+namespace _01_Algoritmos_Y_Estructuras.LeetCode.Stack
+{
+    public class MaximumNestingDepthParentheses_1614
+    {
+        public int MaxDepth(string s)
+        {
+            int count = 0;
+            int max = 0;
+            foreach (char c in s)
+            {
+                if (c == '(') count++;
+                if (c == ')') count--;
+                if (count > max) max = count;
+            }
+            return max;
+        }
+    }
+}
